1524bafa1f3324f9f99eb4f1d8fb49986c6e4ce5
Ram <mramanathan@users.noreply.github.com>
2017-06-29 06:10:22 +0530

    Create helo.py

---
+++ helo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def greet(user):
+
+    print("Hello {}, Welcome on-board!!!".format(user))
+    
+    return None
+    
+
+def main():
+
+    greet("Jenkins")
+    
+    return None
+    
+    
+if __name__ == "__main__":
+    main()
