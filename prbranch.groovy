ae7c1bb0bc79cac18a936cb1ae3251fe72ef6612
Ram <mramanathan@users.noreply.github.com>
2017-07-13 10:39:13 +0530

    Update prbranch.groovy

--- prbranch.groovy
+++ prbranch.groovy
@@ -1,16 +1,13 @@
 #!/usr/bin/groovy
 
-def name {
+import hudson.model.*
 
-  import hudson.model.*
+def payloadString = build.buildVariableResolver.resolve("payload")
 
-  def payloadString = build.buildVariableResolver.resolve("payload")
+payloadObject = new groovy.json.JsonSlurper().parseText(payloadString)
 
-  payloadObject = new groovy.json.JsonSlurper().parseText(payloadString)
-
-  prbranch = payloadObject.pull_request.head.ref
+prbranch = payloadObject.pull_request.head.ref
   
-  echo "PR branch, ${prbranch}"
-}
+echo "PR branch, ${prbranch}"
 
 return this;
